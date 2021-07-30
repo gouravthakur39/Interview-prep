7782863e97535123b34ef1f47d6587a4bb3cfdcf
gouravthakur39 <gourav.thakur39@gmail.com>
2021-07-30 23:06:37 +0530

    practice

---
+++ numberSeriesThree.js
@@ -0,0 +1,7 @@
+// 1, 2, 1, 3, 2, 5, 3, 7, 5, 11, 8, 13, 13, 17…..
+
+// This series is a mixture of 2 series fail the odd terms in 
+// this series form a Fibonacci series 
+// and all the even terms are the prime numbers in ascending order
+
+// Write a program to find the Nth term in this series
---
+++ sum.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+        result = float(a) + float(b)
+        return result
+
+print(sum(20, 20.37))
